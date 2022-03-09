820f8560cc905808e928345fc7554fc0de1ec76f
Omkar <Omkar@DESKTOP-VCP4AV6>
2022-03-09 16:14:40 +0530

    9 march 2022

--- src/NineMarch.java
+++ src/NineMarch.java
@@ -1,9 +1,25 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class NineMarch {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver", "./software/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.amazon.in/");
 
+		// driver.get("https://www.google.com/");
+		// driver.navigate().to("https://www.google.com/");
+		// difference between get() and to() methods
+		// driver.navigate().back();
+		// driver.navigate().forward();
+		// driver.navigate().refresh();  
+
+		System.out.println(driver.getTitle());
+		System.out.println(driver.getCurrentUrl());
+		System.out.println(driver.getPageSource());//it will fetch the source of page in the console
+		 driver.close();
 	}
-
+ 
 }

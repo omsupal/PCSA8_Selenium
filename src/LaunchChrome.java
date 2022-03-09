85757df53a05c135506a1c0171f8027aeec327db
Omkar <Omkar@DESKTOP-VCP4AV6>
2022-03-09 15:16:49 +0530

    9 march 2022

--- src/LaunchChrome.java
+++ src/LaunchChrome.java
@@ -1,6 +1,6 @@
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class DemoLaunch {
+public class LaunchChrome {
 	
 	public static void main(String[] args) {
 	
--- src/LaunchFirefox.java
+++ src/LaunchFirefox.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.firefox.GeckoDriverInfo;
 public class LaunchFirefox {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		System.setProperty("webdriver.gecko.driver", "./software/geckodriver.exe");
 		FirefoxDriver driver= new FirefoxDriver();
 		driver.get("https://www.makemytrip.com/");

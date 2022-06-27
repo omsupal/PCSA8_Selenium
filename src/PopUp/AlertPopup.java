d06df8f1cecce3b1d97cf02f753d0e14bb2c42bd
Omkar <Omkar@DESKTOP-VCP4AV6>
2022-06-27 21:33:01 +0530

    27th

--- src/PopUp/AlertPopup.java
+++ src/PopUp/AlertPopup.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class AlertPopup {
-
+	
 	public static void main(String[] args) {
 		System.setProperty("webdriver.chrome.driver", "./software/chromedriver.exe");
 		WebDriver driver = new ChromeDriver();

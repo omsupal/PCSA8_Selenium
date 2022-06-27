49d4aa6258c3cc9ac689f4c4c15f1c97e6c011fd
Omkar <Omkar@DESKTOP-VCP4AV6>
2022-06-27 22:40:43 +0530

    .

--- src/ImplictAndExplicit/ActiTimeUsingimplicitWait.java
+++ src/ImplictAndExplicit/ActiTimeUsingimplicitWait.java
@@ -1,3 +1,4 @@
+package ImplictAndExplicit;
 import java.time.Duration;
 
 import org.openqa.selenium.By;
---
+++ src/ImplictAndExplicit/ActitimeWithExpectedConditonInterface.java
@@ -0,0 +1,33 @@
+package ImplictAndExplicit;
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class ActitimeWithExpectedConditonInterface {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "./software/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://demo.actitime.com/login.do");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));// implicit wait
+		driver.findElement(By.id("username")).sendKeys("admin");
+		driver.findElement(By.name("pwd")).sendKeys("manager", Keys.ENTER);
+
+		WebDriverWait ww = new WebDriverWait(driver, Duration.ofSeconds(10));// explicit wait or intelligent wait
+		ww.until(new ExpectedCondition<Boolean>() {
+
+			@Override
+			public Boolean apply(WebDriver dd) {
+				boolean dis = dd.findElement(By.id("logoutLink")).isDisplayed();
+				return dis;
+			}
+		});
+		driver.findElement(By.id("logoutLink")).click();
+	}
+
+}
